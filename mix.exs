c6ab25d7fdb3cc1f74e2c2586faf02736fdfec05
Michael Crumm <mike@crumm.net>
2022-09-08 14:45:38 -0700

    Bump Elixir version requirement to match Req

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Absinthe.Socket.MixProject do
     [
       app: :absinthe_client,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
