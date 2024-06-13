6515c5788a18abfa7363296c8a60a2449407098f
Jim Kane <fastjames@gmail.com>
2024-06-13 14:13:40 -0500

    Update supported elixir versions
    Change CI and mix.exs to indicate that elixir versions 1.13 and above
    are now supported (due to the req version update).

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Absinthe.Socket.MixProject do
     [
       app: :absinthe_client,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
