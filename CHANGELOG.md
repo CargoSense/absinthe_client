c178e4d2daa74e00d9773997d4e7e9bc47d1f02e
Ben Wilson <benwilson512@gmail.com>
2024-06-13 16:17:16 -0400

    version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## v0.1.1 (2024-06-13)
+
+- Support newer req
+
 ## v0.1.0 (2022-10-05)
 
 Initial release
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Absinthe.Socket.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/CargoSense/absinthe_socket"
 
   def project do
